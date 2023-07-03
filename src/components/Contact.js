a23d20c69ba0d99564f1efadf32a86f2c73ba843
Mahak491 <shreemahak01@gmail.com>
2023-07-03 08:22:28 +0530

    form design

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -35,8 +35,31 @@ const Contact = () => {
               <a href={link} target='_blank' rel="noopener noreferrer" className='duration-200 ease-in-out hover:text-rose-400'>{icon}</a>
             ))}
           </div>
-          <div>
-            <form></form>
+          <div className='p-8 text-left w-full'>
+            <form action="" method="POST">
+              <div className=' w-full'>
+                <div className='flex flex-col'>
+                  <label className='capitalize text-sm py-2 font-extralight'>name</label>
+                  <input type='text' name='name' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white'/>
+                </div>
+                <div className='flex flex-col my-2'>
+                  <label className='capitalize text-sm py-2 font-extralight'>phone</label>
+                  <input type='text' name='phone' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white'/>
+                </div>
+                <div className='flex flex-col my-2'>
+                  <label className='capitalize text-sm py-2 font-extralight'>email</label>
+                  <input type='text' name='email' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white'/>
+                </div>
+                <div className='flex flex-col'>
+                  <label className='capitalize text-sm py-2 font-extralight'>message</label>
+                  <textarea name='message' rows="10" className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400 dark:bg-gray-900 dark:text-white resize-none' >
+                  </textarea>
+                </div>
+              </div>
+              <div className='flex items-center justify-center'>
+                <button className='my-8 bg-gradient-to-r from-rose-400 to-teal-500 text-white px-6 py-3 uppercase rounded-md tracking-wider cursor-pointer'>send message</button>
+              </div>
+            </form>
           </div>
       </div>
     </Section>
