c070458ddf1e492548927967b54afd156a414505
Mahak491 <shreemahak01@gmail.com>
2024-10-23 12:23:27 +0530

    updated exp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Section className="min-h-fit flex-col justify-start items-center py-16 px-5 text-center">
         <p className='max-w-xl font-light text-gray-500 mb-10'>
-        ©️ 2023 Mahak Shree
+        ©️ 2024 Mahak Shree
         </p>
     </Section>
   )
